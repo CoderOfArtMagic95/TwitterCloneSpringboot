43ca7b6172d4f8cae3f63317dbaecc516fc30098
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-03-31 23:54:26 -0400

    Following and unFollowing Updates to UserController.java and users.html files

--- src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
@@ -22,19 +22,6 @@ public class UserController {
     @Autowired
     private TweetService tweetService;
     
-    private void SetTweetCounts(List<User> users, Model model) {
-    	/*  Within SetTweetCounts under our new HashMap,
-    	 *  we need to iterate through each user, getting a
-    	 *  list of their tweets and adding the size of that
-    	 *  list to the HashMap.*/
-        HashMap<String,Integer> tweetCounts = new HashMap<>();
-        for (User user : users) {
-            List<Tweet> tweets = tweetService.findAllByUser(user);
-            tweetCounts.put(user.getUsername(), tweets.size());
-        }
-        model.addAttribute("tweetCounts", tweetCounts);
-    }
-    
     //add more code here
     @GetMapping(value = "/users/{username}")
     public String getUser(@PathVariable(value="username") String username, Model model) {	
@@ -68,8 +55,42 @@ public class UserController {
     @GetMapping(value = "/users")
     public String getUsers(Model model) {
         List<User> users = userService.findAll();
+        User loggedInUser = userService.getLoggedInUser();
+        List<User> usersFollowing = loggedInUser.getFollowing();
+        SetFollowingStatus(users, usersFollowing, model);
         model.addAttribute("users", users);
         SetTweetCounts(users, model);
         return "users";
     }
+    
+    private void SetTweetCounts(List<User> users, Model model) {
+    	/*  Within SetTweetCounts under our new HashMap,
+    	 *  we need to iterate through each user, getting a
+    	 *  list of their tweets and adding the size of that
+    	 *  list to the HashMap.*/
+        HashMap<String,Integer> tweetCounts = new HashMap<>();
+        for (User user : users) {
+            List<Tweet> tweets = tweetService.findAllByUser(user);
+            tweetCounts.put(user.getUsername(), tweets.size());
+        }
+        model.addAttribute("tweetCounts", tweetCounts);
+    }
+   
+    private void SetFollowingStatus(List<User> users, List<User> usersFollowing, Model model) {
+      //adds a HashMap to the Model
+    	HashMap<String,Boolean> followingStatus = new HashMap<>();
+        String username = userService.getLoggedInUser().getUsername();
+       /*  iterate though each user and check to see whether or not they're
+        *  being followed. Then we add each result to the HashMap, and add 
+        *  the HashMap to the model*/
+        for (User user : users) {
+            if(usersFollowing.contains(user)) {
+                followingStatus.put(user.getUsername(), true);
+            }else if (!user.getUsername().equals(username)) {
+                followingStatus.put(user.getUsername(), false);
+        	}
+        }
+        model.addAttribute("followingStatus", followingStatus);
+    }
+	
 }
