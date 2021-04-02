c6e06b1c450b159b6046fac9f057db0b87860ddc
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-04-01 23:49:57 -0400

    added updates to TweetRepository and TweetService dealing with hashtags

--- src/main/java/com/tts/TechTalentTwitter/repository/TweetRepository.java
+++ src/main/java/com/tts/TechTalentTwitter/repository/TweetRepository.java
@@ -13,6 +13,10 @@ public interface TweetRepository extends CrudRepository<Tweet, Long> {
    /* This is what interacts with the database and allows us to convert between
     *  tweet objects in our code and rows in the tweet table*/
 	List<Tweet> findAllByOrderByCreatedAtDesc();
-    List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
-    List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
+    
+	List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+    
+	List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
+    
+	List<Tweet> findByTags_PhraseOrderByCreatedAtDesc(String phrase);
 }
--- src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
+++ src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
@@ -1,13 +1,18 @@
 package com.tts.TechTalentTwitter.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tts.TechTalentTwitter.model.Tweet;
 import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.model.Tag;
 import com.tts.TechTalentTwitter.repository.TweetRepository;
+import com.tts.TechTalentTwitter.repository.TagRepository;
 
 @Service
 public class TweetService {
@@ -17,7 +22,7 @@ public class TweetService {
     //finds all tweets
     public List<Tweet> findAll() {
         List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
-        return tweets;
+        return formatTweets(tweets);
     }
 	//finds all tweets made by specific one user with User class object user
     public List<Tweet> findAllByUser(User user) {
@@ -31,6 +36,24 @@ public class TweetService {
     }
 	//saves the tweet message
     public void save(Tweet tweet) {
+    	handleTags(tweet);
         tweetRepository.save(tweet);
+    }
+    
+    private void handleTags(Tweet tweet) {
+        List<Tag> tags = new ArrayList<Tag>();
+        Pattern pattern = Pattern.compile("#\\w+");
+        Matcher matcher = pattern.matcher(tweet.getMessage());
+        while (matcher.find()) {
+            String phrase = matcher.group().substring(1).toLowerCase();
+            Tag tag = tagRepository.findByPhrase(phrase);
+            if (tag == null) {
+                tag = new Tag();
+                tag.setPhrase(phrase);
+                tagRepository.save(tag);
+            }
+            tags.add(tag);
+        }
+        tweet.setTags(tags);
     }
 }
